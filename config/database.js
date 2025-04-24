2c801b4bee75e3c646d460b488f5868c47749bc1
leviwof <ganeshkemoap123@gmail.com>
2025-04-24 11:43:42 +0530

    mongodb upgraded

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose")
 
+
+
+const DB_URL = process.env.MONGODB_URI
 const connectDB = async()=>{
  await mongoose.connect(
-    "mongodb+srv://srank8861:Ip6HWoJJjYBLGeOU@cluster0.nvkkzuf.mongodb.net/JobPortal "
+     DB_URL || "mongodb+srv://Srank01:srankzero@cluster0.nvkkzuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    // "mongodb+srv://srank8861:Ip6HWoJJjYBLGeOU@cluster0.nvkkzuf.mongodb.net/JobPortal "
 )
 }
 
--- server.js
+++ server.js
@@ -222,7 +222,7 @@ connectDB()
   .then(() => {
     console.log("MongoDB connected...");
     app.listen(PORT, () => {
-      console.log("Server is running on port 3000...");
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
